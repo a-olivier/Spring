c1fac4bc6cb23533acae52beb77d29035aea4c7a
aOliver33 <ravioli.atomique@gmail.com>
2015-02-19 15:49:18 +0100

    v1 de la page modification de allZips

--- Gradle/webapp/js/controllers.js
+++ Gradle/webapp/js/controllers.js
@@ -3,33 +3,45 @@
 var graddleFrontControllerz = angular.module('graddleFrontControllerz',
 		[ 'ngResource' ]);
 
-graddleFrontControllerz.controller('gr_controllerz', function($scope, Zips, $modal) {
+graddleFrontControllerz.controller('gr_controllerz.line', function($scope, Zips, $modal) {
 	// /////////////// MODAL ///////////////////////////////
 	$scope.items = [ 'item1', 'item2', 'item3' ];
+	
+	
+	$scope.test = 'controleur général'; 
+	
 	$scope.open = function(size) {
 		var modalInstance = $modal.open({
 			templateUrl : 'myModalContent.html',
 			controller : 'ModalDemoCtrl',
 			size : size,
 			resolve : {
-				items : function() {
-					return $scope.items;
+				zip : function() {
+					console.log( $scope.zips);
+					return $scope.zip;
 				}
 			}
 		});
 
-		modalInstance.result.then(function(selectedItem) {
-			$scope.selected = selectedItem;
+		modalInstance.result.then(function(zip) {
+			$scope.zip  = zip;
 		}, function() {
-			$log.info('Modal dismissed at: ' + new Date());
+			console.info('Modal dismissed at: ' + new Date());
 		});
 	};
+});	
+
+
+
+graddleFrontControllerz.controller('gr_controllerz.main', function($scope, Zips, $modal) {
 	// /////////////// REMPLIR TABLEAU /////////////////////////
 	$scope.getAllZips = function() {
+		$scope.showProgress = true;
 		var allZips = Zips.query(function(Zips) {
 			$scope.listeZip = allZips;
 			console.info("lance ");
 		});
+		$scope.showProgress = false;
 
 	};
 
@@ -38,23 +50,27 @@ graddleFrontControllerz.controller('gr_controllerz', function($scope, Zips, $mod
 /// controleur des modals de modification des zips 
 //////////////////////////////////////////////////////////////////////
 angular.module('graddleFrontControllerz').controller('ModalDemoCtrl',
-		function($scope, $modalInstance, items) {
+		function($scope, $modalInstance, zip) {
 
 			// Please note that $modalInstance represents a modal window
 			// (instance)
 			// dependency.
 			// It is not the same as the $modal service used above.
 
-			$scope.items = items;
-			$scope.selected = {
-				item : $scope.items[0]
-			};
+			$scope.zip = zip;
+			var zipsave = angular.copy(zip);
+			
+			$scope.test = 'dans le controleur de modal'; 
 
 			$scope.ok = function() {
-				$modalInstance.close($scope.selected.item);
+				$modalInstance.close(zip);
 			};
 
 			$scope.cancel = function() {
+				console.log(zipsave)
+				zip.city = zipsave.city;
+				zip.pop = zipsave.pop
+				zip.state = zipsave.state
 				$modalInstance.dismiss('cancel');
 			};
 		});
